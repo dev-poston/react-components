d78844b49c6ee6434ea19dacc8859dc09741984b
Devon Poston <dev.poston@gmail.com>
2021-01-31 01:39:33 -0800

    working on react component

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // TODO
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList list={['Fried Chicken', 'Beer']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+    <ul>
+    {props.list.map(item =>
+      <GroceryListItem stuff={item}/>
+    )}
+    </ul>
+);
+
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>{this.props.stuff}</li>
+    );
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
