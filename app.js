64801f47ebcfdc06eb8b3d1ea649e64bb5bd2397
Devon Poston <dev.poston@gmail.com>
2021-01-31 15:04:14 -0800

    complete react components

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 var App = () => (
   <div>
     <h2>Grocery List</h2>
-    <GroceryList list={['Fried Chicken', 'Beer']}/>
+    <GroceryList list={['Fried Chicken', 'Beer']} />
   </div>
 );
 
 var GroceryList = (props) => (
     <ul>
-    {props.list.map(item =>
-      <GroceryListItem stuff={item}/>
-    )}
+      {props.list.map(item =>
+        <GroceryListItem item={item} />
+      )}
     </ul>
 );
 
@@ -18,13 +18,34 @@ class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onListItemOff() {
+    this.setState({
+      done: !this.state.done
+    });
   }
 
   render() {
+
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
     return (
-      <li>{this.props.stuff}</li>
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)}
+      onMouseLeave={this.onListItemOff.bind(this)}>{this.props.item}</li>
     );
   }
-};
+}
 
 ReactDOM.render(<App />, document.getElementById("app"));
