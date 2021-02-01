b03a3454268ac79756111af7c41736d58e43c4bf
Devon Poston <dev.poston@gmail.com>
2021-02-01 12:03:12 -0800

    refactoring and adding grocery item checked off functionality

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
-// TODO
 var App = () => (
   <div>
     <h2>Grocery List</h2>
-    <GroceryList list={['Fried Chicken', 'Beer']} />
+    <GroceryList list={['Bread', 'Butter']} />
   </div>
-);
+)
 
 var GroceryList = (props) => (
-    <ul>
-      {props.list.map(item =>
-        <GroceryListItem item={item} />
-      )}
-    </ul>
-);
+  <ul>
+    {props.list.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+)
 
 class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      done: false
+      done: false,
+      click: false
     }
-  }
+  };
 
-  onListItemHover() {
+  onItemHover() {
     this.setState({
-      done: !this.state.done
-    });
-  }
+        done: !this.state.done
+      })
+  };
 
-  onListItemOff() {
+  onItemClick() {
     this.setState({
-      done: !this.state.done
-    });
+      click: !this.state.click
+    })
   }
 
-  render() {
+  render(){
 
     var style = {
-      fontWeight: this.state.done ? 'bold' : 'normal'
+      fontWeight: this.state.done ? 'bold' : 'normal',
+      textDecoration: this.state.click ? 'line-through' : 'none'
     };
 
     return (
-      <li style={style} onMouseEnter={this.onListItemHover.bind(this)}
-      onMouseLeave={this.onListItemOff.bind(this)}>{this.props.item}</li>
-    );
-  }
+      <li style={style}
+      onMouseEnter={this.onItemHover.bind(this)}
+      onMouseLeave={this.onItemHover.bind(this)}
+      onClick={this.onItemClick.bind(this)}
+      >{this.props.item}</li>
+    )
+  };
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
